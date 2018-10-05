4cf4bbad9de8a0851a0e1a99dc535f0109ce96df
devwz <wellingtonantonio_zaneze@live.com>
2018-10-05 13:08:01 -0300

    Add domain entities.

---
+++ Lanchonete.Core/Entities/Ingrediente.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lanchonete.Core.Entities
+{
+    public class Ingrediente
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+
+        public static List<Ingrediente> Get()
+        {
+            List<Ingrediente> ingredientes = new List<Ingrediente>()
+            {
+                new Ingrediente() { Id = 1, Titulo = "Alface" },
+                new Ingrediente() { Id = 2, Titulo = "Bacon" },
+                new Ingrediente() { Id = 3, Titulo = "Hambúrger de Carne" },
+                new Ingrediente() { Id = 4, Titulo = "Ovo" },
+                new Ingrediente() { Id = 5, Titulo = "Queijo" }
+            };
+
+            return ingredientes;
+        }
+
+        public static Ingrediente Get(string titulo)
+        {
+            return Ingrediente.Get().Find(p => p.Titulo == titulo);
+        }
+    }
+}
---
+++ Lanchonete.Core/Entities/Lanche.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lanchonete.Core.Entities
+{
+    public class Lanche
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public decimal Preco { get; set; }
+        public List<Ingrediente> Ingredientes { get; set; }
+
+        public static List<Lanche> Get()
+        {
+            List<Lanche> lanches = new List<Lanche>()
+            {
+                new Lanche()
+                {
+                    Id = 1,
+                    Titulo = "X-Bacon",
+                    Preco = 6.50m,
+                    Ingredientes = new List<Ingrediente>()
+                    {
+                        Ingrediente.Get("Bacon"),
+                        Ingrediente.Get("Hambúrger de Carne"),
+                        Ingrediente.Get("Queijo")
+                    }
+                },
+                new Lanche()
+                {
+                    Id = 2,
+                    Titulo = "X-Burger",
+                    Preco = 4.5m,
+                    Ingredientes = new List<Ingrediente>()
+                    {
+                        Ingrediente.Get("Hambúrguer de Carne"),
+                        Ingrediente.Get("Queijo")
+                    }
+                },
+                new Lanche()
+                {
+                    Id = 3,
+                    Titulo = "X-Egg",
+                    Preco = 5.3m,
+                    Ingredientes = new List<Ingrediente>()
+                    {
+                        Ingrediente.Get("Ovo"),
+                        Ingrediente.Get("Hambúrguer de Carne"),
+                        Ingrediente.Get("Queijo")
+                    }
+                },
+                new Lanche()
+                {
+                    Id = 4,
+                    Titulo = "X-Egg Bacon",
+                    Preco = 7.3m,
+                    Ingredientes = new List<Ingrediente>()
+                    {
+                        Ingrediente.Get("Ovo"),
+                        Ingrediente.Get("Bacon"),
+                        Ingrediente.Get("Hambúrguer de Carne"),
+                        Ingrediente.Get("Queijo")
+                    }
+                }
+            };
+
+            return lanches;
+        }
+    }
+}
