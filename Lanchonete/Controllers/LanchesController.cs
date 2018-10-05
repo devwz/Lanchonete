d0b991d9abdf98cdaba96a2523a92312ac534b0c
devwz <wellingtonantonio_zaneze@live.com>
2018-10-05 13:26:52 -0300

    Add lanches controller.

---
+++ Lanchonete/Controllers/LanchesController.cs
@@ -0,0 +1,26 @@
+﻿using Lanchonete.Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lanchonete.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LanchesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<Lanche>> Get()
+        {
+            return Lanche.Get();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Lanche> Get(int id)
+        {
+            return Lanche.Get(id);
+        }
+    }
+}
