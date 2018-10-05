f17b2f43b29ddeeb63eb2bc666ab1de00a47f888
devwz <wellingtonantonio_zaneze@live.com>
2018-10-05 15:12:19 -0300

    Enable cors.

--- Lanchonete/Startup.cs
+++ Lanchonete/Startup.cs
@@ -25,6 +25,14 @@ namespace Lanchonete
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllOrigins",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin();
+                    });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +42,8 @@ namespace Lanchonete
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors("AllowAllOrigins");
 
             app.UseMvc();
         }
