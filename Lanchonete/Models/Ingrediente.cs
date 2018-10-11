53f5c1cdf791dc4c88cb181b34a433149f75ce7d
devwz <wellingtonantonio_zaneze@live.com>
2018-10-11 15:16:33 -0300

    Add models.

---
+++ Lanchonete/Models/Ingrediente.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lanchonete.Models
+{
+    public class Ingrediente
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("titulo")]
+        public string Titulo { get; set; }
+
+        [BsonElement("preco")]
+        public decimal Preco { get; set; }
+    }
+}
---
+++ Lanchonete/Models/Lanche.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lanchonete.Models
+{
+    public class Lanche
+    {
+        [BsonId]
+        public int Id { get; set; }
+
+        [BsonElement("titulo")]
+        public string Titulo { get; set; }
+
+        [BsonElement("preco")]
+        public decimal Preco { get; set; }
+
+        [BsonElement("ingredientes")]
+        public List<Ingrediente> Ingredientes { get; set; }
+    }
+}
